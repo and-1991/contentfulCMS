741f768cecf1d5b3a976179c75854983e5b2bf4a
Yuriy <and-1991@list.ru>
2022-01-17 16:28:45 +0300

    first commit

---
+++ client.js
@@ -0,0 +1,8 @@
+import {createClient} from "contentful";
+
+const client = createClient({
+  space: 'qhwbblms081w',
+  accessToken: '9MrXhuVQK6CBVghTLz8BDMbeHQ0I0PQllKt_lTaTKvA'
+})
+
+export default client;
